27832e5aeab784c34bbd88bbf6f3ac578f3686bc
苏小刚 <35674070+liShiZhensPi@users.noreply.github.com>
2019-07-31 13:08:34 +0800

    Update baidu-translate.el

--- baidu-translate.el
+++ baidu-translate.el
@@ -1,33 +1,5 @@
-;; 百度翻译语言列表
-;; auto	自动检测
-;; zh	中文
-;; en	英语
-;; yue	粤语
-;; wyw	文言文
-;; jp	日语
-;; kor	韩语
-;; fra	法语
-;; spa	西班牙语
-;; th	泰语
-;; ara	阿拉伯语
-;; ru	俄语
-;; pt	葡萄牙语
-;; de	德语
-;; it	意大利语
-;; el	希腊语
-;; nl	荷兰语
-;; pl	波兰语
-;; bul	保加利亚语
-;; est	爱沙尼亚语
-;; dan	丹麦语
-;; fin	芬兰语
-;; cs	捷克语
-;; rom	罗马尼亚语
-;; slo	斯洛文尼亚语
-;; swe	瑞典语
-;; hu	匈牙利语
-;; cht	繁体中文
-;; vie	越南语
+;;baidu-translate.el
+;;2019/7/31 by LiShiZhensPi
 
 (defvar TRANS_API_HOST "http://api.fanyi.baidu.com/api/trans/vip/translate")
 ;;需要去百度申请API
@@ -98,6 +70,36 @@ Result: {\"from\":\"zh\",\"to\":\"en\",\"trans_result\":[{\"src\":\"\\u4f60\\u59
     (beginning-of-buffer)))
 
 ;;对内核函数的包装UI，你可以根据需要修改语言类型参数，定制自己的UI，下面2个例子是将region或者buffer中的文字翻译为中文
+;; 百度翻译语言列表
+;; auto	自动检测
+;; zh	中文
+;; en	英语
+;; yue	粤语
+;; wyw	文言文
+;; jp	日语
+;; kor	韩语
+;; fra	法语
+;; spa	西班牙语
+;; th	泰语
+;; ara	阿拉伯语
+;; ru	俄语
+;; pt	葡萄牙语
+;; de	德语
+;; it	意大利语
+;; el	希腊语
+;; nl	荷兰语
+;; pl	波兰语
+;; bul	保加利亚语
+;; est	爱沙尼亚语
+;; dan	丹麦语
+;; fin	芬兰语
+;; cs	捷克语
+;; rom	罗马尼亚语
+;; slo	斯洛文尼亚语
+;; swe	瑞典语
+;; hu	匈牙利语
+;; cht	繁体中文
+;; vie	越南语
 (defun baidu-translate-zh-mark (start end)
   "Translate the marked text to Chinese."
   (interactive "r")
@@ -107,3 +109,5 @@ Result: {\"from\":\"zh\",\"to\":\"en\",\"trans_result\":[{\"src\":\"\\u4f60\\u59
   "Translate the whole buffer to Chinese."
   (interactive)
   (baidu-translate-string (buffer-string) "auto" "zh"))
+
+;;end
